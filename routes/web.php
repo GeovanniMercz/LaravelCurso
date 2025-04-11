832ca636c4e6a507ae2a840ce869598499ea22c1
MerczA <geza.mercz@cceo.com.mx>
2025-04-11 12:00:52 -0600

    feat():Add two new views index and test and cretae the routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*Route::get('/test',function(){
+    return "Welcome";
+});*/
+
+Route::get('/test',function(){
+    return view('test');
+});
+Route::get('/crudy',function(){
+    $age = 21;
+    $data = ['name' => 'Andres', 'age' => $age];
+    return view('crud/index',$data);
+})->name('crud');
+
+
