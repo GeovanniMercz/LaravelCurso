7cfdb97f6ee888e9a924d15102b17d7022d81b8f
MerczA <geza.mercz@cceo.com.mx>
2025-04-16 14:18:26 -0600

    feat ✨Create the function to edit to be able to update the infromation

--- app/Http/Controllers/Dashboard/PostController.php
+++ app/Http/Controllers/Dashboard/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Post\PutRequest;
 use App\Http\Requests\Post\StoreRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
@@ -88,15 +89,17 @@ class PostController extends Controller
      */
     public function edit(Post $post)
     {
-        //
+        $categories = Category::pluck('id','title');
+        return view('dashboard.post.edit',compact('categories', 'post'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Post $post)
+    public function update(PutRequest $request, Post $post)
     {
-        //
+        $post -> update($request->validated ());
+        return to_route ('post.index');
     }
 
     /**
