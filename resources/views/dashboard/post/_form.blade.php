d3f19023e19cf4f5a19112689038f262e5dbed0f
MerczA <geza.mercz@cceo.com.mx>
2025-04-17 10:32:58 -0600

    Fix 🐛 Add the old function to save the information in the create and update function and don't get lost if there is an error

--- resources/views/dashboard/post/_form.blade.php
+++ resources/views/dashboard/post/_form.blade.php
@@ -2,17 +2,17 @@
 
 <div class="">
     <label for="">Title</label>
-    <input type="text" name="title" value="{{ $post->title }}">
+    <input type="text" name="title" value="{{ old('title', $post->title) }}">
 </div>
 
 <div>
     <label for="">Slug</label>
-    <input type="text" name="slug"value="{{ $post->slug }}">
+    <input type="text" name="slug"value="{{ old('slug', $post->slug) }}">
 </div>
 
 <div>
     <label for="">Content</label>
-    <textarea name="content">{{ $post->content }}</textarea>
+    <textarea name="content">{{ old('content', $post->content) }}</textarea>
 
 </div>
 
@@ -20,7 +20,8 @@
     <label for="">Category</label>
     <select name="category_id">
         @foreach ($categories as $title => $id)
-            <option {{ $post->category->id == $id ? 'selected' : '' }} value="{{ $id }}">{{ $title }}
+            <option {{ old('category_id',$post->category_id) == $id ? 'selected' : '' }} value="{{ $id }}">
+                {{ $title }}
             </option>
         @endforeach
     </select>
@@ -28,14 +29,14 @@
 
 <div>
     <label for="">Description</label>
-    <textarea name="description">{{ $post->description }}</textarea>
+    <textarea name="description">{{ old('description',$post->description) }}</textarea>
 </div>
 
 <div>
     <label for="">Posted</label>
     <select name="posted">
-        <option {{ $post->posted == 'yes' ? 'selected' : '' }} value="yes">YES</option>
-        <option {{ $post->posted == 'not' ? 'selected' : '' }} value="not">NO</option>
+        <option {{ old('posted',$post->posted) == 'yes' ? 'selected' : '' }} value="yes">YES</option>
+        <option {{old('posted', $post->posted) == 'not' ? 'selected' : '' }} value="not">NO</option>
 
     </select>
 </div>
