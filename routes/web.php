1d27422a1b04697ada831048367cc0a0573ff0f3
MerczA <geza.mercz@cceo.com.mx>
2025-04-21 13:19:23 -0600

    feat ✨ Creation of the grouped paths and add the category seeder to generate tests within the application

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,14 @@
 use App\Http\Controllers\Dashboard\CategoryController;
 use App\Http\Controllers\Dashboard\PostController;
 use App\Http\Controllers\PrimerControlador;
+use Illuminate\Routing\RouteGroup;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('post',PostController::class);
-Route::resource('category',CategoryController::class);
-
-
-
+Route::group(['prefix'=>'dashboard'],function () {
+    Route::resource('post', PostController::class);
+    Route::resource('category', CategoryController::class);
+});
